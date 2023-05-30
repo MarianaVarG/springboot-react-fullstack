9a036923d34cf1f9c830a0761ee000a2c35101ca
MarianaVG <marianavarelagomez18@gmail.com>
2023-05-30 11:44:35 +0200

    Adding java doc for Main class

--- backendcursojava/src/main/java/com/solera/backendcursojava/BackendcursojavaApplication.java
+++ backendcursojava/src/main/java/com/solera/backendcursojava/BackendcursojavaApplication.java
@@ -13,31 +13,55 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+
+/**
+ * The type Backend curso java application.
+ */
 @SpringBootApplication
 @EnableJpaAuditing // to create Created Date
 @ComponentScan
 public class BackendcursojavaApplication {
 
+	/**
+	 * The Logger.
+	 */
 	static final Logger logger = LoggerFactory.getLogger(BackendcursojavaApplication.class);
 
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BackendcursojavaApplication.class, args);
 		logger.info("Working c:");
 	}
 
 	/**
-	* A single instance is created for the ENTIRE project.
-	*/
+	 * A single instance is created for the ENTIRE project.
+	 *
+	 * @return the b crypt password encoder
+	 */
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
 
+	/**
+	 * Spring application context spring application context.
+	 *
+	 * @return the spring application context
+	 */
 	@Bean
 	public SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
 	}
 
+	/**
+	 * Gets app properties.
+	 *
+	 * @return the app properties
+	 */
 	@Bean(name = "AppProperties")
 	public AppProperties getAppProperties() {
 		return new AppProperties();
@@ -45,7 +69,8 @@ public class BackendcursojavaApplication {
 
 	/***
 	 * In order not to instantiate several mappers
-	 * @return ModelMapper
+	 *
+	 * @return ModelMapper model mapper
 	 */
 	@Bean
 	public ModelMapper modelMapper (){
